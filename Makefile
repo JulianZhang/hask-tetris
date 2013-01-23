7c928c014f3b7fbcc34d2eabcacdef89f5924769
xingtao <peng.pxt@gmail.com>
2013-01-24 00:08:26 +0800

    now, preview area need precise calculate. transform part need using current position. left right boundaries are not included.

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ all:
 	ghc Main.hs Signal.hs Layout.hs Logic.hs Structure.hs Render.hs --make -O2 -o go.exe
 
 clean:
-	rm *.hi *.o go.exe -f
+	rm *.hi *.o go.exe
--- Render.hs
+++ Render.hs
@@ -10,35 +10,37 @@ import Layout(maxRows,maxColumns)
 -- settledColor = grey = Brown = (165, 42, 42) = (0.65, 0.16, 0.16, 1.0)
 
 -- get the coordinate used in 
-coordinateTransform :: Double -> Double -> Position -> (Double, Double, Double, Double)
-coordinateTransform wUnit hUnit p = ( (fromIntegral $ xp p) * wUnit, (fromIntegral $ yp p) * hUnit,
-                                       wUnit, hUnit )
+coordinateTransform :: Position -> (Double, Double, Double, Double)
+coordinateTransform p = ( (fromIntegral $ xp p) * wUnit, (fromIntegral $ yp p) * hUnit, wUnit, hUnit )
 
 drawRectUnit :: (Double, Double, Double, Double) -> Render ()
 drawRectUnit (x, y, w, h) =  rectangle x y w h >> fillPreserve >> stroke
 
+backupCoorTransform :: Double -> Double -> Position -> (Double, Double, Double, Double)
+backupCoorTransform w h p = ( (fromIntegral $ xp p) * (w/20), (fromIntegral $ yp p) + (h/2), wUnit/4, hUnit/4 ) 
+
 tetrisPreviewRender field dw w h = renderWithDrawable dw $ do
                    let block = backupBlock field
-                       wUnit = w / 5
-                       hUnit = h / 5
-                       coors = map (coordinateTransform wUnit hUnit) $ coordinate block -- we get the 4 coordinates
+                       coors = map (backupCoorTransform w h) $ coordinate block -- we get the 4 coordinates
+                       
                        (r,g,b,a) = color block
                    setSourceRGBA r g b a
-                   setLineCap LineCapRound >> setLineJoin LineJoinRound >> (setLineWidth (hUnit / 15))
-                   translate (0) (0)
+                   setLineCap LineCapRound >> setLineJoin LineJoinRound >> (setLineWidth 4)
+                   --translate (0) (20)
                    mapM drawRectUnit coors
 
 tetrisMainRender field dw w h= renderWithDrawable dw $ do
                 let block = currentBlock field
-                    wUnit = w / (fromIntegral maxColumns)
-                    hUnit = h / (fromIntegral maxRows)
                     (r,g,b,a) = color block
-                    coorBlock = map (coordinateTransform wUnit hUnit) $ coordinate block
-                    coors     = map (coordinateTransform wUnit hUnit) $ markField field
+                    coorBlock = map coordinateTransform $ coordinate block
+                    coors     = map coordinateTransform $ markField field
                 setSourceRGBA r g b a
-                setLineCap LineCapRound >> setLineJoin LineJoinRound >> (setLineWidth $ hUnit / 200)
+                setLineCap LineCapRound >> setLineJoin LineJoinRound >> (setLineWidth 4)
                 mapM drawRectUnit coorBlock
 
                 setSourceRGBA 0.65 0.16 0.16 1.0 -- set to greyish color
-                setLineCap LineCapRound >> setLineJoin LineJoinRound >> (setLineWidth $ hUnit / 180)
+                setLineCap LineCapRound >> setLineJoin LineJoinRound >> (setLineWidth 4)
                 mapM drawRectUnit coors
+
+hUnit = 22
+wUnit = 22
